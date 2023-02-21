c8f14f98a19f12ba35c769a8510efa9218582bdb
SelkoSays <70065171+SelkoSays@users.noreply.github.com>
2023-02-21 16:28:03 +0100

    Manjši popravki #3

--- gravitacija.py
+++ gravitacija.py
@@ -1,4 +1,3 @@
-# print("OIS je zakon!")
 # CM/(r+v)^2 = g
 C = 6.674e-11
 M = 5.972e24
@@ -6,4 +5,4 @@ R = 6.371e6
 
 
 def g_pospesek(v: float | int) -> float | int:
-    return 6.674e-11 * 5.972e24 / (6371e3 + v)**2
+    return C * M / (R + v)**2
