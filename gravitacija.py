5b02d31aef41863c8ccdd75d02ef0424fde4d2f3
schtrudl <125376055+schtrudl@users.noreply.github.com>
2023-02-21 17:18:58 +0100

    ker sm zelo len

--- gravitacija.py
+++ gravitacija.py
@@ -4,17 +4,26 @@ M = 5.972e24
 R = 6.371e6
 
 
-def g_pospesek(v: float | int) -> float | int:
+def g_pospesek(v):
     return C * M / (R + v)**2
 
 
 print("OIS je zakon!")
 
 
-def izpis(v, g):
-    print(f"Na nadmorska višina: {v} je gravitacijski pospešek {g}")
+def izpis(v, g, unit="m"):
+    print(
+        f"Na nadmorska višina: {v:.1f} {unit} je gravitacijski pospešek {g:.2f} m/s^2".replace(".", ","))
 
 
-v = eval(input("Vnesite nadmorsko višino: "))
-g = g_pospesek(v)
-izpis(v, g)
+# v = eval(input("Vnesite nadmorsko višino: "))
+# arg naj bo v km
+def finale(v):
+    g = g_pospesek(v*1000)
+    izpis(v/1000, g, unit="km")
+
+
+finale(0.0)
+finale(10.0)
+finale(1000.0)
+finale(100000.0)
